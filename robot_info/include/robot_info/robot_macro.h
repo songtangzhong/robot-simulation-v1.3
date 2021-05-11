bc752a4cb272a52100c11c5344b17dbcca4cdafd
Song Tangzhong <songtangzhong@163.com>
2021-05-11 10:24:36 +0800

    robot_info

--- robot_info/include/robot_info/robot_macro.h
+++ robot_info/include/robot_info/robot_macro.h
@@ -1,17 +1,19 @@
 #ifndef ROBOT_MACRO_H_
 #define ROBOT_MACRO_H_
 
+// Note: "stz" should be replaced with your own user name, and following so.
+
 #define ARM_DOF 7
-#define ARM_SHM_FILE "/dev/robot_arm_shm"
-#define ARM_SEM_FILE "/dev/robot_arm_sem"
+#define ARM_SHM_FILE "/home/stz/robot_files/robot_arm_shm" 
+#define ARM_SEM_FILE "/home/stz/robot_files/robot_arm_sem"
 
 #ifdef USE_END_EFFECTOR
 #define END_EFF_DOF 2
-#define END_EFF_SHM_FILE "/dev/robot_end_effector_shm"
-#define END_EFF_SEM_FILE "/dev/robot_end_effector_sem"
+#define END_EFF_SHM_FILE "/home/stz/robot_files/robot_end_effector_shm"
+#define END_EFF_SEM_FILE "/home/stz/robot_files/robot_end_effector_sem"
 #endif
 
-#define ROBOT_STATE_SHM_FILE "/dev/robot_state_shm"
-#define ROBOT_STATE_SEM_FILE "/dev/robot_state_sem"
+#define ROBOT_STATE_SHM_FILE "/home/stz/robot_files/robot_state_shm"
+#define ROBOT_STATE_SEM_FILE "/home/stz/robot_files/robot_state_sem"
 
 #endif
