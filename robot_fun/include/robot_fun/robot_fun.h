7dc23ba683d99161b90733925a6793f076d4d2f0
Song Tangzhong <songtangzhong@163.com>
2021-05-11 17:34:50 +0800

    robot_fun

--- robot_fun/include/robot_fun/robot_fun.h
+++ robot_fun/include/robot_fun/robot_fun.h
@@ -17,7 +17,7 @@ public:
     RobotFun(const std::string & node_name);
     ~RobotFun();
 
-    void get_arm_joint_positions(double * positions);
+    void get_arm_joint_positions(std::vector<double> & positions);
     void get_arm_joint_velocities(double * velocities);
     void get_arm_joint_efforts(double * efforts);
 
